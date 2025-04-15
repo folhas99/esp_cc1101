36dd30f4fa128d362078ff0d6c93fdc728403d9f
folhas99 <50446713+folhas99@users.noreply.github.com>
2025-04-15 17:57:34 +0100

    2

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -8,22 +8,31 @@ CODEOWNERS = ["@folhas99"]
 cc1101_ns = cg.esphome_ns.namespace("cc1101")
 CC1101Component = cc1101_ns.class_("CC1101Component", cg.Component)
 
+CONF_SCK_PIN = "sck_pin"
+CONF_MISO_PIN = "miso_pin"
+CONF_MOSI_PIN = "mosi_pin"
+CONF_CSN_PIN = "csn_pin"
+CONF_GDO0_PIN = "gdo0_pin"
+CONF_GDO2_PIN = "gdo2_pin"
+CONF_BANDWIDTH = "bandwidth"
+CONF_FREQUENCY = "frequency"
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101Component),
-    cv.Required("sck_pin"): cv.int_,
-    cv.Required("miso_pin"): cv.int_,
-    cv.Required("mosi_pin"): cv.int_,
-    cv.Required("csn_pin"): cv.int_,
-    cv.Required("gdo0_pin"): cv.int_,
-    cv.Required("gdo2_pin"): cv.int_,  # <--- adiciona isto aqui!
-    cv.Required("bandwidth"): cv.float_,
-    cv.Required("frequency"): cv.float_,
+    cv.Required(CONF_SCK_PIN): cv.int_,
+    cv.Required(CONF_MISO_PIN): cv.int_,
+    cv.Required(CONF_MOSI_PIN): cv.int_,
+    cv.Required(CONF_CSN_PIN): cv.int_,
+    cv.Required(CONF_GDO0_PIN): cv.int_,
+    cv.Required(CONF_GDO2_PIN): cv.int_,
+    cv.Required(CONF_BANDWIDTH): cv.float_,
+    cv.Required(CONF_FREQUENCY): cv.float_,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID],
-        config["sck_pin"], config["miso_pin"], config["mosi_pin"],
-        config["csn_pin"], config["gdo0_pin"], config["gdo2_pin"],
-        config["bandwidth"], config["frequency"]
+        config[CONF_SCK_PIN], config[CONF_MISO_PIN], config[CONF_MOSI_PIN],
+        config[CONF_CSN_PIN], config[CONF_GDO0_PIN], config[CONF_GDO2_PIN],
+        config[CONF_BANDWIDTH], config[CONF_FREQUENCY]
     )
     await cg.register_component(var, config)
