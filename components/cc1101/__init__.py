91cfd909251967f2327fb758feb18ab4cf9be2eb
folhas99 <50446713+folhas99@users.noreply.github.com>
2025-04-15 18:01:11 +0100

    3

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -8,14 +8,24 @@ CODEOWNERS = ["@folhas99"]
 cc1101_ns = cg.esphome_ns.namespace("cc1101")
 CC1101Component = cc1101_ns.class_("CC1101Component", cg.Component)
 
-CONF_SCK_PIN = "sck_pin"
-CONF_MISO_PIN = "miso_pin"
-CONF_MOSI_PIN = "mosi_pin"
-CONF_CSN_PIN = "csn_pin"
-CONF_GDO0_PIN = "gdo0_pin"
-CONF_GDO2_PIN = "gdo2_pin"
-CONF_BANDWIDTH = "bandwidth"
-CONF_FREQUENCY = "frequency"
+
+#CONF_SCK_PIN = "sck_pin"
+#CONF_MISO_PIN = "miso_pin"
+#CONF_MOSI_PIN = "mosi_pin"
+#CONF_CSN_PIN = "csn_pin"
+#CONF_GDO0_PIN = "gdo0_pin"
+#CONF_GDO2_PIN = "gdo2_pin"
+#CONF_BANDWIDTH = "bandwidth"
+#CONF_FREQUENCY = "frequency"
+
+CONF_SCK_PIN = 18
+CONF_MISO_PIN = 19
+CONF_MOSI_PIN = 23
+CONF_CSN_PIN = 5
+CONF_GDO0_PIN = 32
+CONF_GDO2_PIN = 33
+CONF_BANDWIDTH = 200
+CONF_FREQUENCY = 433.92
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101Component),
