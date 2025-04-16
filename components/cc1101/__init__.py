b4643495c7ea7e681663777046956a915a0327bf
folhas99 <50446713+folhas99@users.noreply.github.com>
2025-04-16 12:36:26 +0100

    test 3

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,1 +1,35 @@
-# required for esphome to recognize this component
+from esphome.components import sensor
+import esphome.config_validation as cv
+import esphome.codegen as cg
+
+cc1101_ns = cg.esphome_ns.namespace('cc1101')
+CC1101Component = cc1101_ns.class_('CC1101Component', cg.PollingComponent, sensor.Sensor)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CC1101Component),
+    cv.Required("sck_pin"): cv.gpio_pin,
+    cv.Required("miso_pin"): cv.gpio_pin,
+    cv.Required("mosi_pin"): cv.gpio_pin,
+    cv.Required("cs_pin"): cv.gpio_pin,
+    cv.Required("gdo0_pin"): cv.gpio_pin,
+    cv.Required("gdo2_pin"): cv.gpio_pin,
+    cv.Required("bandwidth_khz"): cv.float_,
+    cv.Required("freq_mhz"): cv.float_,
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+
+    cg.add(var.set_pins(
+        config["sck_pin"].number,
+        config["miso_pin"].number,
+        config["mosi_pin"].number,
+        config["cs_pin"].number,
+        config["gdo0_pin"].number,
+        config["gdo2_pin"].number
+    ))
+
+    cg.add(var.set_bandwidth(config["bandwidth_khz"]))
+    cg.add(var.set_frequency(config["freq_mhz"]))
